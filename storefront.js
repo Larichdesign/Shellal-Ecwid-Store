fe713abdef50d0469f95b40707710cb540c8b726
Atlan Design <80818227+Larichdesign@users.noreply.github.com>
2025-12-06 14:54:17 +0100

    Refactor checkout icon loading and remove Stripe terms hiding
    Removed the functionality to hide Stripe terms text and simplified the CheckoutIconLoad function.

--- storefront.js
+++ storefront.js
@@ -1,47 +1,17 @@
-var client_id = "custom-app-123237799-1";
-var image_link = "https://iili.io/fT8bjLv.png";
+var client_id = "custom-app-123237799-1"
+var image_link = "https://iili.io/fT8bjLv.png"
 
-// ================================
-// 1) Add custom image to payment method
-// ================================
+// function that adds an image
 var CheckoutIconLoad = function () {
-   var icon = "<div class='icon_resizer' style='height:22px; overflow:hidden'> <img style='width:auto; height:100%; display:block;' src='" + image_link + "'></img> </div>";
-   var labelTarget = document.querySelector("label.ec-radiogroup__item--app_id-" + client_id + " div.ec-radiogroup__info:empty");
-   if (labelTarget) {
-      labelTarget.innerHTML = icon;
-   }
-};
+   var icon = "<div class='icon_resizer' style='height:22px; overflow:hidden'> <img style='width:auto; height:100%; display:block;' src='"+image_link+"'></img> </div>";
+   document.querySelector("label.ec-radiogroup__item--app_id-"+client_id+" div.ec-radiogroup__info:empty").innerHTML = icon;
+}
 
-// ================================
-// 2) Hide Stripe Terms Text
-// ================================
-var HideStripeTerms = function () {
-   var interval = setInterval(function () {
-      var termsEl = document.querySelector('.de7zKgf4__p-Root');
-
-      if (termsEl) {
-         termsEl.style.display = "none";
-
-         var wrapper = termsEl.closest('.p-Grid');
-         if (wrapper) wrapper.style.display = "none";
-
-         clearInterval(interval);
-      }
-   }, 200);
-};
-
-// ================================
-// 3) Ecwid event listener
-// ================================
+// call function on the page load
 Ecwid.OnAPILoaded.add(function () {
    Ecwid.OnPageLoaded.add(function (page) {
-      if (page.type === "CHECKOUT_PAYMENT_DETAILS") {
+      if (page.type == "CHECKOUT_PAYMENT_DETAILS") {
          CheckoutIconLoad();
-         HideStripeTerms();
       }
    });
 });
-
-
-
-
