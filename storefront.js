89207768a7ec748308a1d9bce72420e22f2aac14
Atlan Design <80818227+Larichdesign@users.noreply.github.com>
2025-12-06 08:55:08 +0100

    Refactor CheckoutIconLoad function for clarity

--- storefront.js
+++ storefront.js
@@ -1,38 +1,17 @@
-var client_id = "custom-app-123237799-1";
-var image_link = "https://iili.io/fz8RtDv.png";
+var client_id = "custom-app-123237799-1"
+var image_link = "https://iili.io/fz8RtDv.png"
 
-// Add icon to payment method
-function CheckoutIconLoad() {
-    // Icon wrapper
-    var iconHTML = `
-        <div class="icon_resizer" style="height:40px; overflow:hidden;">
-            <img src="${image_link}" style="width:auto; height:100%; display:block;" />
-        </div>
-    `;
-
-    // Target Ecwid payment method selector
-    var selector = "label.ec-radiogroup__item--app_id-" + client_id + " .ec-radiogroup__info";
-
-    // Get the element
-    var target = document.querySelector(selector);
-
-    // Fallback: wait for element to appear
-    if (!target) {
-        setTimeout(CheckoutIconLoad, 300);
-        return;
-    }
-
-    // Prevent duplicate icons
-    if (target.innerHTML.trim() === "") {
-        target.innerHTML = iconHTML;
-    }
+// function that adds an image
+var CheckoutIconLoad = function () {
+   var icon = "<div class='icon_resizer' style='height:40px; overflow:hidden'> <img style='width:auto; height:100%; display:block;' src='"+image_link+"'></img> </div>";
+   document.querySelector("label.ec-radiogroup__item--app_id-"+client_id+" div.ec-radiogroup__info:empty").innerHTML = icon;
 }
 
-// Ecwid Hooks
+// call function on the page load
 Ecwid.OnAPILoaded.add(function () {
-    Ecwid.OnPageLoaded.add(function (page) {
-        if (page.type === "CHECKOUT_PAYMENT_DETAILS") {
-            CheckoutIconLoad();
-        }
-    });
+   Ecwid.OnPageLoaded.add(function (page) {
+      if (page.type == "CHECKOUT_PAYMENT_DETAILS") {
+         CheckoutIconLoad();
+      }
+   });
 });
