c3c7f8375d2938417ebfd2685a5c5577eb35f8f1
Atlan Design <80818227+Larichdesign@users.noreply.github.com>
2025-12-06 14:27:53 +0100

    Refactor CheckoutIconLoad and HideStripeTerms functions

--- storefront.js
+++ storefront.js
@@ -5,7 +5,7 @@ var image_link = "https://iili.io/fT8bjLv.png";
 // 1) Add custom image to payment method
 // ================================
 var CheckoutIconLoad = function () {
-   var icon = "<div class='icon_resizer' style='height:30px; overflow:hidden'> <img style='width:auto; height:100%; display:block;' src='" + image_link + "'></img> </div>";
+   var icon = "<div class='icon_resizer' style='height:22px; overflow:hidden'> <img style='width:auto; height:100%; display:block;' src='" + image_link + "'></img> </div>";
    var labelTarget = document.querySelector("label.ec-radiogroup__item--app_id-" + client_id + " div.ec-radiogroup__info:empty");
    if (labelTarget) {
       labelTarget.innerHTML = icon;
@@ -41,4 +41,5 @@ Ecwid.OnAPILoaded.add(function () {
       }
    });
 });
+
 
